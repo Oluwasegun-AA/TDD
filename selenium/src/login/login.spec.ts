6d7014c1bb2ac30411e0ed7b24c2a87727abf883
shegsteham <oluwasegunadepoju@gmail.com>
2019-11-26 09:00:16 +0200

    test login and signup path

---
+++ selenium/src/login/login.spec.ts
@@ -0,0 +1,24 @@
+import { By } from 'selenium-webdriver';
+import dotenv from 'dotenv';
+import driver from '../../config/index';
+
+dotenv.config();
+const { WEBSITE_URL } = process.env;
+
+const login: any = async (): Promise<any> => {
+  // try {
+  await driver.get(WEBSITE_URL);
+  const element = await driver.findElement(By.className('login'));
+  await element.click();
+  const username = await driver.findElement(By.className('username'));
+  const password = await driver.findElement(By.className('password'));
+  const submitBtn = await driver.findElement(By.className('loginButtonDiv')).findElement(By.css('button'));
+  await username.sendKeys('shegsteham');
+  await password.sendKeys('p@ssword123');
+  await submitBtn.click();
+  // } finally {
+  //   await driver.quit();
+  // }
+};
+
+login();
