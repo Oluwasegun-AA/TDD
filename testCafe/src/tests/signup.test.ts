4167e60c31a80cd7f34cda0e15ad7af1e0eb291d
shegsteham <oluwasegunadepoju@gmail.com>
2019-11-27 12:55:11 +0200

    ft(signup):test signup path

--- testCafe/src/tests/signup.test.ts
+++ testCafe/src/tests/signup.test.ts
@@ -3,18 +3,13 @@ import setupFixture from '../common/index';
 
 setupFixture('WEBSITE_URL');
 
-test('navigation bar', async (view: any) => {
-  const blinkText = await Selector('.blink').innerText;
-  const text = await Selector('.text').innerText;
-  await view.expect(text).eql('Great Taste Always');
-  await view.expect(blinkText).eql('Always');
-});
-
 test('login button', async (view: any) => {
-  await view.click('.login')
+  await view.click('.signup')
     .typeText('.username', 'shegs')
+    .typeText('.email', 'oluwasegunadepoju@gmail.com')
     .typeText('.password', 'password')
+    .typeText('#confirmPassword', 'password')
     .click('.button.loginBtn');
   const loginPageText = await Selector('#notMember').find('span').innerText;
-  await view.expect(loginPageText).eql('Not a member? ');
+  await view.expect(loginPageText).eql('Already a member? ');
 });
