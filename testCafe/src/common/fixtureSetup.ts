641e7822df393a873ad7eab288d2db365f2c733b
shegsteham <oluwasegunadepoju@gmail.com>
2019-11-27 17:11:54 +0200

    refactor fixture setup

--- testCafe/src/common/fixtureSetup.ts
+++ testCafe/src/common/fixtureSetup.ts
@@ -2,9 +2,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const setupFixture: any = (ENV_URL: string) => {
+const setupFixture: any = (description: string, ENV_URL: string) => {
   const WEBSITE_URL = process.env[ENV_URL];
-  return fixture`Home page test`.page`${WEBSITE_URL}`;
+  return fixture(description).page(`${WEBSITE_URL}`);
 };
 
 export default setupFixture;
